4cdfe8c7574dd69cecd4014b6756f8699c830e8c
Amandalfs <amandalfs57@gmail.com>
2025-01-09 18:58:02 -0300

    feat: create database en

---
+++ src/app/api/[locale]/bestprojects/route.ts
@@ -0,0 +1,33 @@
+import { promises as fs } from 'fs';
+import { NextResponse } from 'next/server';
+import { BestProjectsDto } from './bestProjects.dto';
+
+interface Params {
+  locale: string;
+}
+
+interface ErrorResponse {
+  error: string;
+}
+
+interface BestProjectsResponse {
+  bestProjects: BestProjectsDto;
+}
+
+export async function GET(_: unknown, { params }: { params: Promise<Params> }): Promise<NextResponse<BestProjectsResponse | ErrorResponse>> {
+  try {
+    const locale = (await params).locale;
+    console.log(locale);
+
+    if (!["en", "pt"].includes(locale)) {
+      return NextResponse.json({ error: 'Locale not found' }, { status: 404 });
+    }
+
+    const data = await fs.readFile(`./src/app/_database/${locale}/database.json`, 'utf8');
+    const bestProjects = JSON.parse(data);
+    return NextResponse.json({ bestProjects });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'Failed to load data' }, { status: 500 });
+  }
+}
