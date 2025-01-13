b21ad22e75f99d7b041804d3d954507c8a710e9e
Amandalfs <amandalfs57@gmail.com>
2025-01-13 20:56:08 -0300

    css: create css to desktop

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,15 +10,15 @@ import Computer from "@/components/imagens/computer";
 export default function Hero(){
     const translate = useTranslations("hero");
      
-    return (<section id="home" className="flex flex-col justify-center items-center mt-16">
+    return (<section id="home" className="flex flex-col justify-center items-center mt-16 md:gap-32 md:flex-row-reverse">
         <Computer className="text-secondaryColor-light dark:text-secondaryColor-dark" />
-        <article className="flex flex-col gap-6 mx-6">
+        <article className="flex flex-col gap-6 mx-6 md:w-[592px]">
             <h1 className="text-[40px] font-kalam font-medium leading-10 bg-text-gradient dark:bg-text-gradient-dark bg-clip-text text-transparent">{translate("title")}</h1>
             <article className="flex flex-col">
                 <Typography.H2>{translate("subtitle")}</Typography.H2>
                 <Typography.paragraph>{translate("description")}</Typography.paragraph>
             </article>
-            <article className="w-full flex flex-row gap-4 justify-center">
+            <article className="w-full flex flex-row gap-4 justify-center md:justify-start">
                 <Button color="secondary">
                     <DocIcon className="fill-textSecondary-light dark:fill-textSecondary-dark"/>
                     {translate("button-about")}
@@ -35,6 +35,7 @@ export default function Hero(){
                 absolute
                 bottom-[-120px]
                 right-32
+                md:right-[-45%]
                 "/>
             </article>
         </article>
