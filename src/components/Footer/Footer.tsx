97ffcdf8fc87a749ab100ea9d37e78f1e8cf4ce3
Amandalfs <amandalfs57@gmail.com>
2025-02-10 14:36:15 -0300

    feat: create action arrow up page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Link } from "@/i18n/routing";
 import { Typography } from "../ui/typography";
 import ArrowUp from "@/components/icons/ArrowUp";
@@ -17,6 +18,9 @@ export default function Footer() {
             </li>
             <li>
                 <ArrowUp
+                    onClick={()=>{ 
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                    }}
                     className="fill-textPrimary-light dark:fill-textPrimary-dark"
                 />
             </li>
